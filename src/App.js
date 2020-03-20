5fb4c9828276df55872beb2aa5b298f1df15bea4
aagrawal3 <aagrawal3@atlassian.com>
2020-03-20 18:27:11 +0530

    Class based react component with Functional Component.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/person';
 
 class App extends Component {
+  state = {
+      persons : [
+          {name : "abhi", age: "100"},
+          {name : "hello", age: "90"},
+          {name : "James", age: "65"}
+      ]
+  };
+
+  switchNameHandler = () =>{
+      this.setState({persons: [{name : "abhishek", age: "33"},{name : "hello", age: "90"},
+              {name : "James", age: "65"}]});
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hi, I am a React App</h1>
+        <p>React App Working</p>
+        <button onClick={this.switchNameHandler}>Click Me!</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies Are: Reading and Entreprenurship</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
   }
